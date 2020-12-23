8c0f064ada70a5b0bd86e8819421212612b94b0c
PercyJiang <hjiang@wpi.edu>
2020-12-22 22:09:56 -0500

    controller hello

---
+++ server/src/main/java/com/example/server/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.example.server.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+@AllArgsConstructor
+public class UserController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello";
+    }
+
+}
