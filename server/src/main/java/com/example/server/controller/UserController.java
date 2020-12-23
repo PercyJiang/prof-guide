05c3623bd1196916c898cb9b530716a13af4f5c9
PercyJiang <hjiang@wpi.edu>
2020-12-22 22:25:27 -0500

    user signup

--- server/src/main/java/com/example/server/controller/UserController.java
+++ server/src/main/java/com/example/server/controller/UserController.java
@@ -1,18 +1,28 @@
 package com.example.server.controller;
 
+import com.example.server.dto.UserSignupRequest;
+import com.example.server.service.UserService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user")
 @AllArgsConstructor
 public class UserController {
 
+    private final UserService userService;
+
     @GetMapping("/hello")
     public String hello() {
         return "Hello";
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<String> signup(@RequestBody UserSignupRequest request) {
+        userService.signup(request);
+        return new ResponseEntity<>("User Signup Success", HttpStatus.OK);
     }
 
 }
--- server/src/main/java/com/example/server/service/UserService.java
+++ server/src/main/java/com/example/server/service/UserService.java
@@ -1,4 +1,26 @@
 package com.example.server.service;
 
+import com.example.server.dto.UserSignupRequest;
+import com.example.server.model.User;
+import com.example.server.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+
+@Service
+@AllArgsConstructor
+@Transactional
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public void signup(UserSignupRequest request) {
+        User user = new User();
+        user.setUsername(request.getUsername());
+        user.setPassword(request.getPassword());
+        user.setCreated(Instant.now());
+        userRepository.save(user);
+    }
 }
